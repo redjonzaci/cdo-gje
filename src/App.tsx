7dfd88ae78bf4ef0ccd4683483c8ee12b2be667e
redjonzaci <redjonzaci@gmail.com>
2022-05-20 11:23:18 +0200

    Add theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { Button } from '@geist-ui/core';
+import CssBaseline from '@geist-ui/core/esm/css-baseline';
+import GeistProvider from '@geist-ui/core/esm/geist-provider';
+import { useState } from 'react';
 import './App.css';
 import Home from './pages/Home';
 
 function App() {
-  return <Home />;
+  const [themeType, setThemeType] = useState('light');
+  const switchThemes = () => {
+    setThemeType((last) => (last === 'dark' ? 'light' : 'dark'));
+  };
+
+  return (
+    <>
+      <GeistProvider themeType={themeType}>
+        <CssBaseline />
+        <Button onClick={switchThemes}>
+          {themeType === 'light' ? 'Nate' : 'Dite'}
+        </Button>
+        <Home />
+      </GeistProvider>
+    </>
+  );
 }
 
 export default App;
