40550a14a1a1cada0f2893d2f52f8e8ff21e60dc
redjonzaci <redjonzaci@gmail.com>
2022-05-20 17:37:11 +0200

    Update theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Button } from '@geist-ui/core';
 import CssBaseline from '@geist-ui/core/esm/css-baseline';
 import GeistProvider from '@geist-ui/core/esm/geist-provider';
+import Grid from '@geist-ui/core/esm/grid';
+import Moon from '@geist-ui/icons/moon';
+import Sun from '@geist-ui/icons/sun';
 import { useState } from 'react';
 import './App.css';
 import Home from './pages/Home';
@@ -15,9 +18,17 @@ function App() {
     <>
       <GeistProvider themeType={themeType}>
         <CssBaseline />
-        <Button onClick={switchThemes}>
-          {themeType === 'light' ? 'Nate' : 'Dite'}
-        </Button>
+        <Grid.Container justify="flex-end">
+          <Button
+            auto
+            iconRight={themeType === 'light' ? <Moon /> : <Sun />}
+            marginRight={1}
+            marginTop={1}
+            onClick={switchThemes}
+          >
+            {themeType === 'light' ? 'Nate' : 'Dite'}
+          </Button>
+        </Grid.Container>
         <Home />
       </GeistProvider>
     </>
