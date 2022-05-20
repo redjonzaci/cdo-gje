d39731b90e8fd9bd95556265703c6fffa6a4fc42
redjonzaci <redjonzaci@gmail.com>
2022-05-20 11:41:29 +0200

    Add Search

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,32 @@
+import Button from '@geist-ui/core/esm/button';
+import Input from '@geist-ui/core/esm/input';
 import Text from '@geist-ui/core/esm/text';
+import CornerDownLeft from '@geist-ui/icons/cornerDownLeft';
+import Search from '@geist-ui/icons/search';
+import { KeyboardEvent, MouseEvent } from 'react';
 
 function Home() {
+  function handleEnter(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      alert('Duke kerkuar...');
+    }
+  }
+
+  function handleClick(event: MouseEvent) {
+    alert('Duke kerkuar...');
+  }
+
   return (
     <>
       <Text h1>Cdo Gje</Text>
+      <Input
+        iconRight={<CornerDownLeft />}
+        onKeyDown={handleEnter}
+        placeholder="Shtepi, Pune, Makina, ..."
+      />
+      <Button icon={<Search />} onClick={handleClick}>
+        Kerko
+      </Button>
     </>
   );
 }
