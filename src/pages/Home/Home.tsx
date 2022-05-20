c300fc853ccecf71f79a0917de5184fcfb39fbdb
redjonzaci <redjonzaci@gmail.com>
2022-05-20 16:47:02 +0200

    Add dummy items

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,27 @@
 import Button from '@geist-ui/core/esm/button';
+import Card from '@geist-ui/core/esm/card';
+import Grid from '@geist-ui/core/esm/grid';
 import Input from '@geist-ui/core/esm/input';
+import Spacer from '@geist-ui/core/esm/spacer';
 import Text from '@geist-ui/core/esm/text';
 import CornerDownLeft from '@geist-ui/icons/cornerDownLeft';
 import Search from '@geist-ui/icons/search';
-import { KeyboardEvent, MouseEvent } from 'react';
+import { KeyboardEvent, MouseEvent, useState } from 'react';
 
 function Home() {
+  const items = Array.apply(null, Array(64)).map((item, index) => {
+    return { index };
+  });
+  function generateListOf(items: any[]) {
+    return items.map((items, index) => {
+      return (
+        <Grid key={index} xs={20}>
+          <Card width={100}>{index}</Card>
+        </Grid>
+      );
+    });
+  }
+
   function handleEnter(event: KeyboardEvent) {
     if (event.key === 'Enter') {
       alert('Duke kerkuar...');
@@ -27,6 +43,15 @@ function Home() {
       <Button icon={<Search />} onClick={handleClick}>
         Kerko
       </Button>
+      <Spacer />
+      <Grid.Container gap={2}>
+        <Grid xs={8} width={100}></Grid>
+        <Grid.Container xs={16} direction="column" alignItems="center">
+          <Grid.Container gap={2} justify="center">
+            {generateListOf(items)}
+          </Grid.Container>
+        </Grid.Container>
+      </Grid.Container>
     </>
   );
 }
