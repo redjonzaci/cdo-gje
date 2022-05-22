b0d561039c21c57ad20f858b7ccda2524d098f92
redjonzaci <redjonzaci@gmail.com>
2022-05-22 13:50:29 +0200

    Add Preferences drawer

--- src/pages/Home/Header/Header.tsx
+++ src/pages/Home/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/css';
 import DarkMode from '@mui/icons-material/DarkMode';
 import LightMode from '@mui/icons-material/LightMode';
 import Button from '@mui/material/Button';
@@ -15,8 +16,21 @@ function Header({
 }) {
   return (
     <header>
-      <Grid container justifyContent="flex-end">
-        <Grid item marginTop={2} marginRight={2}>
+      <Grid container justifyContent="flex-end" marginTop={2} gap={2}>
+        <Grid item>
+          <Button
+            className={css({
+              '&': {
+                marginRight: '2rem',
+              },
+            })}
+            onClick={() => setIsSettingsVisible(true)}
+            variant="outlined"
+          >
+            Preferencat
+          </Button>
+        </Grid>
+        <Grid item marginRight={2}>
           <Button onClick={switchThemes} variant="outlined">
             {themeType === 'light' ? <DarkMode /> : <LightMode />}
           </Button>
--- src/pages/Home/Preferences/Preferences.tsx
+++ src/pages/Home/Preferences/Preferences.tsx
@@ -1,4 +1,11 @@
-import { css } from '@emotion/css';
+import Drawer from '@mui/material/Drawer';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
 
 function Preferences({
   isSettingsVisible,
@@ -29,7 +36,30 @@ function Preferences({
     }
   }
 
-  return <aside></aside>;
+  return (
+    <aside>
+      <Drawer
+        anchor="right"
+        onClose={() => setIsSettingsVisible(false)}
+        open={isSettingsVisible}
+      >
+        <List>
+          <ListItem>
+            <FormControl>
+              <FormLabel>Postime per rresht</FormLabel>
+              <RadioGroup
+                value={10 / itemWidth}
+                onChange={(event) => changeLayout(event.target.value)}
+              >
+                <FormControlLabel value="2" control={<Radio />} label="2" />
+                <FormControlLabel value="4" control={<Radio />} label="4" />
+              </RadioGroup>
+            </FormControl>
+          </ListItem>
+        </List>
+      </Drawer>
+    </aside>
+  );
 }
 
 export default Preferences;
