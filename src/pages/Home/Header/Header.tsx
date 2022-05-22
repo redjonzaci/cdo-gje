c38c9ca3e6912aa6dc97df25ea5a74c793ebe6f1
redjonzaci <redjonzaci@gmail.com>
2022-05-22 18:12:19 +0200

    Add post form trigger

--- src/pages/Home/Header/Header.tsx
+++ src/pages/Home/Header/Header.tsx
@@ -8,14 +8,24 @@ function Header({
   themeType,
   switchThemes,
   setIsSettingsVisible,
+  setIsPostFormVisible,
 }: {
   themeType: string;
   switchThemes: MouseEventHandler;
   setIsSettingsVisible: Function;
+  setIsPostFormVisible: Function;
 }) {
   return (
     <header>
       <Grid container justifyContent="flex-end" marginTop={2} gap={2}>
+        <Grid item marginRight={2}>
+          <Button
+            onClick={() => setIsPostFormVisible(true)}
+            variant="contained"
+          >
+            Shto postim
+          </Button>
+        </Grid>
         <Grid item marginRight={2}>
           <Button onClick={() => setIsSettingsVisible(true)} variant="outlined">
             Preferencat
