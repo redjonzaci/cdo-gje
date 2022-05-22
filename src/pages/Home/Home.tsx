da30811a361c4d8ed88d7c31ff2eeeb7e2ec6842
redjonzaci <redjonzaci@gmail.com>
2022-05-22 18:11:50 +0200

    Add post form visibility state

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,15 +15,22 @@ function Home({
   const [itemWidth, setItemWidth] = useState(5);
   const [itemsPerPage, setItemsPerPage] = useState(2 * rowsPerPage);
   const [isSettingsVisible, setIsSettingsVisible] = useState(false);
+  const [isPostFormVisible, setIsPostFormVisible] = useState(false);
 
   return (
     <>
       <Header
         themeType={themeType}
         switchThemes={switchThemes}
+        setIsPostFormVisible={setIsPostFormVisible}
         setIsSettingsVisible={setIsSettingsVisible}
       />
-      <Main itemsPerPage={itemsPerPage} itemWidth={itemWidth} />
+      <Main
+        itemsPerPage={itemsPerPage}
+        itemWidth={itemWidth}
+        isPostFormVisible={isPostFormVisible}
+        setIsPostFormVisible={setIsPostFormVisible}
+      />
       <Preferences
         isSettingsVisible={isSettingsVisible}
         setIsSettingsVisible={setIsSettingsVisible}
