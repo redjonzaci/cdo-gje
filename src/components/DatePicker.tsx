a9cd9c6467a591454e28ab11ae2f1a05703972fd
redjonzaci <redjonzaci@gmail.com>
2022-05-22 11:34:25 +0200

    Remove React DayPicker

--- src/components/DatePicker.tsx
+++
@@ -1,7 +0,0 @@
-import { DayPicker } from 'react-day-picker';
-import 'react-day-picker/dist/style.css';
-import sq from 'date-fns/locale/sq';
-
-export default function DatePicker() {
-  return <DayPicker locale={sq} />;
-}
