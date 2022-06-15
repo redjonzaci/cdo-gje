3baa9024d13f2606a58694c9224ca8102506da11
redjonzaci <redjonzaci@gmail.com>
2022-06-15 16:07:22 +0200

    Add House interface

---
+++ models/House.ts
@@ -0,0 +1,10 @@
+export default interface House {
+  title: string;
+  description: string;
+  cityId: number;
+  houseTypeId: number;
+  houseCategoryId: number;
+  surface: number;
+  price: number;
+  currencyId: number;
+}
